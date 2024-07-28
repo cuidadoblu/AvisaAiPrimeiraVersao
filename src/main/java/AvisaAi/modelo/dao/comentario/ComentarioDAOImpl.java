f1a85b5452a571a4ff363b8524a8d29d717838dc
Willian Filipe <William.stela@hotmail.com>
2024-07-28 14:45:22 -0300

    Criada interface ComentarioDAO e classe ComentarioDAOImpl

---
+++ src/main/java/AvisaAi/modelo/dao/comentario/ComentarioDAOImpl.java
@@ -0,0 +1,22 @@
+package AvisaAi.modelo.dao.comentario;
+
+import java.util.List;
+
+public abstract class ComentarioDAOImpl implements ComentarioDAO {
+	
+	void inserirComentario(Comentario comentario) {
+		
+	}
+
+	void deletarComentario(Comentario comentario) {
+		
+	}
+
+	void atualizarComentario(Comentario comentario) {
+		
+	}
+
+	List<Comentario> recuperarComentarios() {
+		
+	}
+}
