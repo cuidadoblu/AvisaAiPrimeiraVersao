70c0b4b63477da244b8b407a043c1615f7ce51d1
Gabriel Andrei Junges <gabriel.a.junges@hotmail.com>
2024-08-02 15:01:44 -0300

    Atualizado o JPA da Classe Comunidade e implementada interface Serializable

--- src/main/java/AvisaAi/modelo/entidade/comunidade/Comunidade.java
+++ src/main/java/AvisaAi/modelo/entidade/comunidade/Comunidade.java
@@ -1,32 +1,52 @@
-package modelo.entidade.comunidade;
+package AvisaAi.modelo.entidade.comunidade;
 
+import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
-import modelo.entidade.incidente.Incidente;
+import AvisaAi.modelo.entidade.incidente.Incidente;
+import AvisaAi.modelo.entidade.localidade.Localidade;
+import AvisaAi.modelo.entidade.usuario.Usuario;
 
 @Entity
-@Table(name = "Comunidade")
-public class Comunidade  {
-	
+@Table(name = "comunidade")
+public class Comunidade implements Serializable {
+
+	private static final long serialVersionUID = -9216497520338583127L;
+
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTIFY)
 	@Column(name = "id_comunidade")
-	private int idComunidade;
+	private Long id;
 	
-	@Column(name = "nome_comunidade")
+	@Column(name = "nome_comunidade", lenght = 30, nullable = false)
 	private String nome;
 	
-	@Column(name = "id_localidade")
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_localidade", nullable = false)
 	private Localidade localidade;
 	
-	@Column(name = "id_incidente")
+	@OneToMany(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_incidente", nullable = false)
 	private List<Incidente> incidentes;
 	
-	@Column(name = "id_usuario")
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_usuario", nullable = false)
 	private List<Usuario> usuarios;
+	
+	public Comunidade() {}
 	
 	public Comunidade(String nome, Localidade localidade) {
 		setNome(nome);
 		setLocalidade(localidade);
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
 	}
 	
 	public String getNome() {
@@ -51,6 +71,21 @@ public class Comunidade  {
 	
 	public List<Usuario> getUsuario() {
 		return usuarios;
+	}
+
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comunidade other = (Comunidade) obj;
+		return Objects.equals(id, other.id);
 	}
  
 }
