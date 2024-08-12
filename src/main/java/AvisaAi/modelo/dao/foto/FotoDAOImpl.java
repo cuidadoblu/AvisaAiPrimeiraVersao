c9d0543b72ad5761bc303275a6438b9e48a2619f
Willian Filipe <William.stela@hotmail.com>
2024-08-12 20:27:21 -0300

    Implementado conexao, e metodos DAO

--- src/main/java/AvisaAi/modelo/dao/foto/FotoDAOImpl.java
+++ src/main/java/AvisaAi/modelo/dao/foto/FotoDAOImpl.java
@@ -1,20 +1,168 @@
 package AvisaAi.modelo.dao.foto;
 
+import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import org.hibernate.Session;
+
+import AvisaAi.modelo.entidade.foto.Foto;
+import AvisaAi.modelo.factory.conexao.ConexaoFactory;
+
 public class FotoDAOImpl implements FotoDAO {
 	
-	public void inserirFoto(Foto foto) {
-		
-	}
+	  private ConexaoFactory fabrica;
+	    
+	    public FotoDAOImpl() {
+	        this.fabrica = new ConexaoFactory();
+	    }
+	    
+	    public void inserirFoto(Foto foto) {
+	    	
+	        Session sessao = null;
+	        
+	        try {
+	        	
+	        	sessao = fabrica.getConexao().openSession();
+	            sessao.beginTransaction();
+	            
+	            sessao.save(foto);
+	            
+	            sessao.getTransaction().commit();
+	            
+	        } catch (Exception sqlException) {
+	        	
+	            if (sessao.getTransaction() != null) {
+	                sessao.getTransaction().rollback();
+	            }
+	            sqlException.printStackTrace();
+	            
+	        } finally {
+	        	
+	            if (sessao != null) {
+	                sessao.close();
+	            }
+	        }
+	    }
 
-	public void deletarFoto(Foto foto) {
-		
-	}
-	
-	public void atualizarFoto(Foto foto) {
-		
-	}
+	    public void deletarFoto(Foto foto) {
+	    	
+	        Session sessao = null;
+	        
+	        try {
+	        	
+	        	sessao = fabrica.getConexao().openSession();
+	            sessao.beginTransaction();
+	            
+	            sessao.remove(foto);
+	            
+	            sessao.getTransaction().commit();
+	            
+	        } catch (Exception sqlException) {
+	        	
+	            if (sessao.getTransaction() != null) {
+	                sessao.getTransaction().rollback();
+	            }
+	            sqlException.printStackTrace();
+	            
+	        } finally {
+	        	
+	            if (sessao != null) {
+	                sessao.close();
+	            }
+	        }
+	    }
 
-	public List<Foto> recuperarFotos(){
-		
-	}
+	    public void atualizarFoto(Foto foto) {
+	    	
+	        Session sessao = null;
+	        
+	        try {
+	        	
+	        	sessao = fabrica.getConexao().openSession();
+	            sessao.beginTransaction();
+	            
+	            sessao.update(foto);
+	            
+	            sessao.getTransaction().commit();
+	            
+	        } catch (Exception sqlException) {
+	        	
+	            if (sessao.getTransaction() != null) {
+	                sessao.getTransaction().rollback();
+	            }
+	            sqlException.printStackTrace();
+	            
+	        } finally {
+	        	
+	            if (sessao != null) {
+	                sessao.close();
+	            }
+	        }
+	    }
+
+	    public List<Foto> recuperarFotos() {
+	    	
+	        Session sessao = null;
+	        List<Foto> fotos = null;
+	        
+	        try {
+	        	
+	        	sessao = fabrica.getConexao().openSession();
+	            sessao.beginTransaction();
+	            
+	            CriteriaBuilder construtor = sessao.getCriteriaBuilder();
+	            CriteriaQuery<Foto> criteria = construtor.createQuery(Foto.class);
+	            
+	            Root<Foto> raizFoto = criteria.from(Foto.class);
+	            criteria.select(raizFoto);
+	            
+	            fotos = sessao.createQuery(criteria).getResultList();
+	            sessao.getTransaction().commit();
+	            
+	        } catch (Exception sqlException) {
+	        	
+	            if (sessao.getTransaction() != null) {
+	                sessao.getTransaction().rollback();
+	            }
+	            sqlException.printStackTrace();
+	            
+	        } finally {
+	        	
+	            if (sessao != null) {
+	                sessao.close();
+	            }
+	        }
+	        
+	        return fotos;
+	    }
+
+	    public Foto consultarFotoId(Long id) {
+	    	
+	        Session sessao = null;
+	        Foto foto = null;
+	        
+	        try {
+	        	
+	            sessao = fabrica.getConexao().openSession();
+	            sessao.beginTransaction();
+	            foto = sessao.get(Foto.class, id);
+	            sessao.getTransaction().commit();
+	            
+	        } catch (Exception sqlException) {
+	        	
+	            if (sessao.getTransaction() != null) {
+	                sessao.getTransaction().rollback();
+	            }
+	            sqlException.printStackTrace();
+	            
+	        } finally {
+	        	
+	            if (sessao != null) {
+	                sessao.close();
+	            }
+	        }
+	        
+	        return foto;
+	    }
 }
