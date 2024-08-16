d75d5d174c54cbc5c1c3b5145191b2bd1fe42ceb
Gabriel Andrei Junges <gabriel.a.junges@hotmail.com>
2024-08-16 14:52:39 -0300

    Alterado imports da classe Localidade

--- src/main/java/AvisaAi/modelo/entidade/localidade/Localidade.java
+++ src/main/java/AvisaAi/modelo/entidade/localidade/Localidade.java
@@ -1,13 +1,17 @@
 package AvisaAi.modelo.entidade.localidade;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import AvisaAi.modelo.entidade.incidente.Incidente;
